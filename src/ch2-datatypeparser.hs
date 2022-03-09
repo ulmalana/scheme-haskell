8f7f9d7dfd566d5c92393928fd6c45e2fee2b267
ulmalana <muhamaul@gmail.com>
2022-03-09 15:19:59 +0700

    [HScheme] finished chapter 2

---
+++ src/ch2-datatypeparser.hs
@@ -0,0 +1,63 @@
+module Main where
+
+import System.Environment
+import Control.Monad
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String 
+             | Bool Bool
+
+main :: IO ()
+main = do 
+    args <- getArgs
+    putStrLn (readExpr (args !! 0))
+
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of 
+    Left err -> "No match: " ++ show err 
+    Right val -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString = do 
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x 
+
+parseAtom :: Parser LispVal
+parseAtom = do 
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let atom = [first] ++ rest 
+    return $ case atom of 
+        "#t" -> Bool True 
+        "#f" -> Bool False
+        otherwise -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+-- exercise 1.a: OK
+-- parseNumber :: Parser LispVal
+-- parseNumber = do 
+--     x <- many1 digit
+--     let y = (Number . read) x 
+--     return y
+
+-- exercise 1.b: OK
+-- parseNumber :: Parser LispVal
+-- parseNumber = many1 digit >>=  return . Number . read
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
+
