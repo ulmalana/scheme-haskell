a22d67fd336fa491ce66102e3fcc4caed239d980
ulmalana <muhamaul@gmail.com>
2022-03-13 14:39:02 +0700

    [HScheme] finished chapter 5

--- src/ch5-operator-parser.hs
+++ src/ch5-operator-parser.hs
@@ -1,9 +1,10 @@
+{-# LANGUAGE ExistentialQuantification #-}
 module Main where
 
 import System.Environment
 import Control.Monad
 import Text.ParserCombinators.Parsec hiding (spaces)
-import Control.Monad.Error
+import Control.Monad.Except
 
 data LispVal = Atom String
              | List [LispVal]
@@ -20,12 +21,13 @@ data LispError = NumArgs Integer [LispVal]
                | UnboundVar String String
                | Default String
 
+data Unpacker = forall a. Eq a => AnyUnpacker (LispVal -> ThrowsError a)
 
 instance Show LispVal where show = showVal
 instance Show LispError where show = showError
-instance Error LispError where
-    noMsg = Default "An error has occurred"
-    strMsg = Default
+-- instance Error LispError where
+--     noMsg = Default "An error has occurred"
+--     strMsg = Default
 
 type ThrowsError = Either LispError
 
@@ -69,10 +71,11 @@ parseList :: Parser LispVal
 parseList = liftM List $ sepBy parseExpr spaces
 
 parseDottedList :: Parser LispVal
-parseDottedList = do 
-    head_ <- endBy parseExpr spaces
-    tail_ <- char '.' >> spaces >> parseExpr
-    return $ DottedList head_ tail_
+parseDottedList = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ DottedList head tail
+
 
 parseQuoted :: Parser LispVal
 parseQuoted = do 
@@ -87,7 +90,7 @@ parseExpr = parseAtom
          <|> parseNumber
          <|> parseQuoted
          <|> do char '('
-                x <- (try parseList) <|> parseDottedList
+                x <- try parseList <|> parseDottedList
                 char ')'
                 return x
 
@@ -108,8 +111,14 @@ eval val@(String _) = return val
 eval val@(Number _) = return val
 eval val@(Bool _) = return val 
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", pred, conseq, alt]) = do 
+    result <- eval pred 
+    case result of 
+        Bool False -> eval alt
+        otherwise -> eval conseq
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
+
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply func args = maybe (throwError $ NotFunction "Unrecognized primitive function args" func) 
@@ -135,7 +144,13 @@ primitives = [("+", numericBinop (+)),
               ("string=?", strBoolBinop (==)),
               ("string?", strBoolBinop (>)),
               ("string>=?", strBoolBinop (>=)),
-              ("string<=?", strBoolBinop (<=))
+              ("string<=?", strBoolBinop (<=)),
+              ("car", car),
+              ("cdr", cdr),
+              ("cons", cons),
+              ("eq?", eqv),
+              ("eqv?", eqv),
+              ("equal?", equal)
              ]
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
 numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
@@ -183,4 +198,54 @@ unpackStr notString = throwError $ TypeMismatch "string" notString
 
 unpackBool :: LispVal -> ThrowsError Bool 
 unpackBool (Bool b) = return b
-unpackBool notBool = throwError $ TypeMismatch "boolean" notBool                                     
+unpackBool notBool = throwError $ TypeMismatch "boolean" notBool 
+
+car :: [LispVal] -> ThrowsError LispVal
+car [List (x:xs)] = return x
+car [DottedList (x:xs) _] = return x 
+car [badArg] = throwError $ TypeMismatch "pair" badArg
+
+cdr :: [LispVal] -> ThrowsError LispVal
+cdr [List (x:xs)] = return $ List xs
+cdr [DottedList [_] x] = return x 
+cdr [DottedList (_ : xs) x] = return $ DottedList xs x 
+cdr [badArg] = throwError $ TypeMismatch "pair" badArg
+cdr badArgList = throwError $ NumArgs 1 badArgList
+
+cons :: [LispVal] -> ThrowsError LispVal
+cons [x1, List []] = return $ List [x1]
+cons [x, List xs] = return $ List $ x : xs
+cons [x, DottedList xs xlast] = return $ DottedList (x : xs) xlast
+cons [x1, x2] = return $ DottedList [x1] x2
+cons badArgList = throwError $ NumArgs 2 badArgList
+
+
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [(Bool arg1), (Bool arg2)]      = return $ Bool $ arg1 == arg2
+eqv [(Number arg1), (Number arg2)]  = return $ Bool $ arg1 == arg2
+eqv [(String arg1), (String arg2)]  = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1), (Atom arg2)]      = return $ Bool $ arg1 == arg2
+eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv [(List arg1), (List arg2)]      = return $ Bool $ (length arg1 == length arg2) &&
+                                                      (all eqvPair $ zip arg1 arg2)
+  where eqvPair (x1, x2) = case eqv [x1, x2] of 
+                                Left err -> False
+                                Right (Bool val) -> val 
+eqv [_, _] = return $ Bool False
+eqv badArgList = throwError $ NumArgs 2 badArgList
+
+unpackEquals :: LispVal -> LispVal -> Unpacker -> ThrowsError Bool 
+unpackEquals arg1 arg2 (AnyUnpacker unpacker) = do 
+    unpacked1 <- unpacker arg1
+    unpacked2 <- unpacker arg2
+    return $ unpacked1 == unpacked2 
+  `catchError` (const $ return False)
+
+equal :: [LispVal] -> ThrowsError LispVal
+equal [arg1, arg2] = do 
+    primitiveEquals <- liftM or $ mapM (unpackEquals arg1 arg2)
+                       [AnyUnpacker unpackNum, AnyUnpacker unpackStr, AnyUnpacker unpackBool]
+    eqvEquals <- eqv [arg1, arg2]
+    return $ Bool $ (primitiveEquals || let (Bool x) = eqvEquals in x)
+equal badArgList = throwError $ NumArgs 2 badArgList
