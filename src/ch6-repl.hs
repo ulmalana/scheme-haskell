505aeb68f3a0c9ca1616cbb1800a2592e9cfa876
ulmalana <muhamaul@gmail.com>
2022-03-14 14:09:17 +0700

    [HScheme] finished chapter 6 - REPL

--- src/ch6-repl.hs
+++ src/ch6-repl.hs
@@ -5,6 +5,7 @@ import System.Environment
 import Control.Monad
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Monad.Except
+import System.IO
 
 data LispVal = Atom String
              | List [LispVal]
@@ -34,8 +35,13 @@ type ThrowsError = Either LispError
 main :: IO ()
 main = do 
     args <- getArgs
-    evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-    putStrLn $ extractValue $ trapError evaled
+    case length args of 
+        0 -> runRepl
+        1 -> evalAndPrint $ args !! 0
+        otherwise -> putStrLn "Program takes only 0 or 1 argument"
+
+    -- evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
+    -- putStrLn $ extractValue $ trapError evaled
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -250,3 +256,26 @@ equal [arg1, arg2] = do
     eqvEquals <- eqv [arg1, arg2]
     return $ Bool $ (primitiveEquals || let (Bool x) = eqvEquals in x)
 equal badArgList = throwError $ NumArgs 2 badArgList
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do 
+    result <- prompt
+    if pred result
+        then return ()
+        else action result >> until_ pred prompt action
+
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "HScheme>>> ") evalAndPrint
